e16370273040cbec3393340018705b7c9bcaac90
chenzr <chenzr@chd.edu.cn>
2019-12-03 16:57:10 +0800

    webgl09

--- Matrix4.js
+++ Matrix4.js
@@ -2,6 +2,9 @@
  * 列主序4x4矩阵
  */
 export class Matrix4 {
+    /**
+     * 以列主序存储
+     */
     elements = new Float32Array([
         1, 0, 0, 0,
         0, 1, 0, 0,
@@ -10,41 +13,101 @@ export class Matrix4 {
     ])
 
     /**
+     * 以行主序设置元素
      * @param {number} m11 
-     * @param {number} m21 
-     * @param {number} m31 
-     * @param {number} m41 
      * @param {number} m12 
-     * @param {number} m22 
-     * @param {number} m32 
-     * @param {number} m42 
      * @param {number} m13 
-     * @param {number} m23 
-     * @param {number} m33 
-     * @param {number} m43 
      * @param {number} m14 
+     * @param {number} m21 
+     * @param {number} m22 
+     * @param {number} m23 
      * @param {number} m24 
+     * @param {number} m31 
+     * @param {number} m32 
+     * @param {number} m33 
      * @param {number} m34 
+     * @param {number} m41 
+     * @param {number} m42 
+     * @param {number} m43 
      * @param {number} m44 
      */
-    set(m11, m21, m31, m41, m12, m22, m32, m42, m13, m23, m33, m43, m14, m24, m34, m44) {
+    set(m11, m12, m13, m14, m21, m22, m23, m24, m31, m32, m33, m34, m41, m42, m43, m44) {
         this.elements[0] = m11
-        this.elements[1] = m12
-        this.elements[2] = m13
-        this.elements[3] = m14
-        this.elements[4] = m21
+        this.elements[1] = m21
+        this.elements[2] = m31
+        this.elements[3] = m41
+        this.elements[4] = m12
         this.elements[5] = m22
-        this.elements[6] = m23
-        this.elements[7] = m24
-        this.elements[8] = m31
-        this.elements[9] = m32
+        this.elements[6] = m32
+        this.elements[7] = m42
+        this.elements[8] = m13
+        this.elements[9] = m23
         this.elements[10] = m33
-        this.elements[11] = m34
-        this.elements[12] = m41
-        this.elements[13] = m42
-        this.elements[14] = m43
+        this.elements[11] = m43
+        this.elements[12] = m14
+        this.elements[13] = m24
+        this.elements[14] = m34
         this.elements[15] = m44
         return this
+    }
+
+    /**
+     * 乘以目标矩阵
+     * @param {Matrix4} matrix4 
+     * @return {this} this
+     */
+    multiply(matrix4) {
+        return this.multiplyMatrices(this, matrix4)
+    }
+
+    /**
+     * 将2个矩阵相乘结果赋给当前矩阵
+     * @param {Matrix4} matA 
+     * @param {Matrix4} matB 
+     */
+    multiplyMatrices(matA, matB) {
+        /**
+         * a11, a12, a13, a14,
+         * a21, a22, a23, a24,
+         * a31, a32, a33, a34,
+         * a41, a42, a43, a44,
+         */
+        const [a11, a21, a31, a41, a12, a22, a32, a42, a13, a23, a33, a43, a14, a24, a34, a44] = matA.elements
+        /**
+         * b11, b12, b13, b14,
+         * b21, b22, b23, b24,
+         * b31, b32, b33, b34,
+         * b41, b42, b43, b44,
+         */
+        const [b11, b21, b31, b41, b12, b22, b32, b42, b13, b23, b33, b43, b14, b24, b34, b44] = matB.elements
+
+        const m11 = a11 * b11 + a12 * b21 + a13 * b31 + a14 * b41
+        const m12 = a11 * b12 + a12 * b22 + a13 * b32 + a14 * b42
+        const m13 = a11 * b13 + a12 * b23 + a13 * b33 + a14 * b43
+        const m14 = a11 * b14 + a12 * b24 + a13 * b34 + a14 * b44
+        const m21 = a21 * b11 + a22 * b21 + a23 * b31 + a24 * b41
+        const m22 = a21 * b12 + a22 * b22 + a23 * b32 + a24 * b42
+        const m23 = a21 * b13 + a22 * b23 + a23 * b33 + a24 * b43
+        const m24 = a21 * b14 + a22 * b24 + a23 * b34 + a24 * b44
+        const m31 = a31 * b11 + a32 * b21 + a33 * b31 + a34 * b41
+        const m32 = a31 * b12 + a32 * b22 + a33 * b32 + a34 * b42
+        const m33 = a31 * b13 + a32 * b23 + a33 * b33 + a34 * b43
+        const m34 = a31 * b14 + a32 * b24 + a33 * b34 + a34 * b44
+        const m41 = a41 * b11 + a42 * b21 + a43 * b31 + a44 * b41
+        const m42 = a41 * b12 + a42 * b22 + a43 * b32 + a44 * b42
+        const m43 = a41 * b13 + a42 * b23 + a43 * b33 + a44 * b43
+        const m44 = a41 * b14 + a42 * b24 + a43 * b34 + a44 * b44
+        return this.set(
+            m11, m12, m13, m14,
+            m21, m22, m23, m24,
+            m31, m32, m33, m34,
+            m41, m42, m43, m44,
+        )
+    }
+
+    toString() {
+        const [m11, m21, m31, m41, m12, m22, m32, m42, m13, m23, m33, m43, m14, m24, m34, m44] = this.elements
+        return `${m11} ${m12} ${m13} ${m14}\n${m21} ${m22} ${m23} ${m24}\n${m31} ${m32} ${m33} ${m34}\n${m41} ${m42} ${m43} ${m44}`
     }
 
     /**
@@ -55,10 +118,10 @@ export class Matrix4 {
      */
     static translation(tx, ty, tz) {
         return new Matrix4().set(
-            1, 0, 0, 0,
-            0, 1, 0, 0,
-            0, 0, 1, 0,
-            tx, ty, tz, 1
+            1, 0, 0, tx,
+            0, 1, 0, ty,
+            0, 0, 1, tz,
+            0, 0, 0, 1,
         )
     }
 
---
+++ webgl09.js
@@ -0,0 +1,63 @@
+import { createWebGLContext, createShaderProgram, rotate } from './utils.js'
+import Matrix4 from './Matrix4.js'
+
+const gl = createWebGLContext()
+
+const VERTEX_SHADER_SOURCE = `
+    precision mediump float;
+    attribute vec2 a_Point;
+    uniform mat4 u_Matrix;
+    uniform vec2 u_Resolution;
+
+    // 坐标系转换函数
+    vec2 convert(vec2 origin) {
+        return origin * vec2(2.0,-2.0) / u_Resolution + vec2(-1.0,1.0);
+    }
+
+    void main() {
+        vec2 xy = convert((u_Matrix * vec4(a_Point,0.0,1.0)).xy);
+        gl_Position = vec4(xy,0.0,1.0);
+    }
+`
+
+const FRAG_SHADER_SOURCE = `
+    precision mediump float;
+    uniform vec2 u_Resolution;
+
+    void main() {
+        gl_FragColor = vec4(gl_FragCoord.xy / u_Resolution,0.0,1.0);
+    }
+`
+
+const program = createShaderProgram(gl, VERTEX_SHADER_SOURCE, FRAG_SHADER_SOURCE)
+gl.useProgram(program)
+
+
+const resolutionLocation = gl.getUniformLocation(program, 'u_Resolution')
+gl.uniform2fv(resolutionLocation, new Float32Array([window.innerWidth, window.innerHeight]))
+const matrixLocation = gl.getUniformLocation(program, 'u_Matrix')
+const rotation = Matrix4.rotationZ(Math.PI / 180 * 45)
+const translation = Matrix4.translation(200, 200, 0)
+const matrix = new Matrix4()
+// mat4(先旋转再平移)*vec4 = mat4(平移)*mat4(旋转)*vec4
+matrix.multiplyMatrices(translation, rotation)
+gl.uniformMatrix4fv(matrixLocation, false, matrix.elements)
+
+const pointLocation = gl.getAttribLocation(program, 'a_Point')
+const buffer = gl.createBuffer()
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+const vertices = new Float32Array([
+    0, 0,
+    0, 200,
+    200, 0
+])
+
+gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
+gl.vertexAttribPointer(pointLocation, 2, gl.FLOAT, false, 0, 0)
+gl.enableVertexAttribArray(pointLocation)
+
+gl.clearColor(0, 0, 0, 1)
+gl.clear(gl.COLOR_BUFFER_BIT)
+
+gl.drawArrays(gl.TRIANGLES, 0, 3)
+
